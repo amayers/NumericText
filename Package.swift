76e6e63b92bfd75f58f90019d38bdbfba652346c
Andrew Mayers <andrew@mayers.us>
2020-09-07 13:24:26 -0400

    Update macOS deployment target
    from v10_16 to v11 since Apple finally got their version numbers correct

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NumericText",
-    platforms: [.iOS(.v14), .macOS(.v10_16), .watchOS(.v7), .tvOS(.v14)],
+    platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v7), .tvOS(.v14)],
     products: [
         .library(
             name: "NumericText",
